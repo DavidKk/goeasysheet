958e65dff363e5dbe6965ad7d41c6faaa7edb6dc
zhongjiahao <zhongjiahao@yy.com>
2019-12-17 18:15:20 +0800

    update

---
+++ scripts/publish.js
@@ -0,0 +1,8 @@
+const path = require('path')
+const cp = require('child_process')
+const branch = cp.execSync('git rev-parse --abbrev-ref HEAD').toString('utf-8').replace(/\n/g, '')
+
+if (branch === 'master') {
+  const sourceDir = path.join(__dirname, '../googlescripts')
+  cp.execSync(`cd ${sourceDir} && clasp push`)
+}
