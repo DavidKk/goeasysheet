d6d7e8bf63329a048f30a7e4f6bd67fa5ca2608f
zhongjiahao <zhongjiahao@yy.com>
2019-12-19 00:03:47 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,37 +1,37 @@
 // 初始化
-function onOpen () {
-  App.install();
-}
+// function onOpen () {
+//   App.install();
+// }
 
 // 每日任务触发器
-function onDaily () {
-  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-  var sheet = spreadsheet.getSheetByName('工作表1');
-  if (sheet == null) {
-    return;
-  }
+// function onDaily () {
+//   var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+//   var sheet = spreadsheet.getSheetByName('工作表1');
+//   if (sheet == null) {
+//     return;
+//   }
 
-  var dateRange = sheet.getRange('M:M');
-  var contentRange = sheet.getRange('D:D');
+//   var dateRange = sheet.getRange('M:M');
+//   var contentRange = sheet.getRange('D:D');
 
-  var dates = dateRange.getValues();
-  var contents = contentRange.getValues();
+//   var dates = dateRange.getValues();
+//   var contents = contentRange.getValues();
 
-  var tasks = [];
-  var now = new Date();
+//   var tasks = [];
+//   var now = new Date();
 
-  contents.forEach(function (item, row) {
-    var date = dates[row][0];
-    var content = item[0];
+//   contents.forEach(function (item, row) {
+//     var date = dates[row][0];
+//     var content = item[0];
 
-    if (content && date instanceof Date && isEqualDate(date, now)) {
-      tasks.push(content)
-    }
-  });
+//     if (content && date instanceof Date && isEqualDate(date, now)) {
+//       tasks.push(content)
+//     }
+//   });
 
-  var email = Session.getActiveUser().getEmail();
-  MailApp.sendEmail(email, '提测提醒', '今天要提测 ' + tasks.join(',') + ' 啦!!!');
-}
+//   var email = Session.getActiveUser().getEmail();
+//   MailApp.sendEmail(email, '提测提醒', '今天要提测 ' + tasks.join(',') + ' 啦!!!');
+// }
 
 // 调试
 function develop () {
